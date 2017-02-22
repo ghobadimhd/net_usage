eda61036135c92baf42d453199807b9c39a77ec5
mamad <ghobadimhd@gmail.com>
2017-02-22 13:31:02 +0330

    url added

--- monitor/urls.py
+++ monitor/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
+from monitor import views
 
 urlpatterns = [
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^netusage/$', views.net_usage)
 ]
