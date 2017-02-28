26d18c18fb87fb9be61330ed73b40d5cc8628a58
mamad <ghobadimhd@gmail.com>
2017-02-28 20:32:01 +0330

    remote report added to view

--- monitor/views.py
+++ monitor/views.py
@@ -4,8 +4,18 @@ from django.shortcuts import render_to_response
 from datetime import date
 from vnstat import vnstat
 
+DEFUALT_REMOTE_PORT = 1234
+
 def net_usage(request, interface=None, traffic_set='days', from_date=date(1, 1, 1), to_date=date.today()):
-	data = vnstat.read()
+	
+	if request.method == 'POST':
+		remote_address = request.POST.get('remote_address','')
+		remote_port = int(request.POST.get('remote_port',-1))
+		remote_port = DEFUALT_REMOTE_PORT if remote_port == -1 else remote_port
+		# check for valid address
+		data = vnstat.remote_read(remote_address, remote_port)
+	else:
+		data = vnstat.read()
 	vnstat.format_data(data)
 	vnstat.convert_unit(data)
 	interfaces = vnstat.get(data, traffic_set, interface, from_date, to_date)
