54c0c475e2b7458cf2394e96f54a87bde5535370
mamad <ghobadimhd@gmail.com>
2017-07-29 02:36:11 +0430

    support remote host monitoring

--- monitor/views.py
+++ monitor/views.py
@@ -1,5 +1,6 @@
 """mointor views"""
 from datetime import date
+from django.http import Http404
 from django.template import Context
 from django.shortcuts import render_to_response
 from vnstat import vnstat
@@ -9,16 +10,18 @@ DEFUALT_REMOTE_PORT = 1234
 def net_usage(request, interface=None, traffic_set='days',
               from_date=date(1, 1, 1), to_date=date.today()):
     """read data from vnstat from localhost or remote and represent """
-    if request.method == 'POST':
-        remote_address = request.POST.get('remote_address', '')
-        remote_port = int(request.POST.get('remote_port', -1))
-        remote_port = DEFUALT_REMOTE_PORT if remote_port == -1 else remote_port
-        # check for valid address
-        data = vnstat.remote_read(remote_address, remote_port)
-    else:
-        data = vnstat.read()
-    vnstat.format_data(data)
-    vnstat.convert_unit(data)
-    interfaces = vnstat.get(data, traffic_set, interface, from_date, to_date)
-    context = Context({'interfaces':interfaces})
-    return render_to_response('net_usage.html', context)
+    if request.method == 'GET':
+
+        remote_address = request.GET.get('remote_address', '')
+        remote_port = int(request.GET.get('remote_port', 1234))
+        if remote_address:
+            # check for valid address
+            data = vnstat.remote_read(remote_address, remote_port)
+        else:
+            data = vnstat.read()
+        vnstat.format_data(data)
+        vnstat.convert_unit(data)
+        interfaces = vnstat.get(data, traffic_set, interface, from_date, to_date)
+        context = Context({'interfaces':interfaces})
+        return render_to_response('net_usage.html', context)
+    raise Http404
